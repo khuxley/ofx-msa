d34b9d04d4c92c0aa67f0cd06295934ddf5f6d0f
memosan <memosan@a9ddf6a8-909b-11dd-94ae-b329dc30fcad>
2009-06-04 11:26:14 +0000

    fixes and tings
    git-svn-id: http://ofxmsaof.googlecode.com/svn/trunk@111 a9ddf6a8-909b-11dd-94ae-b329dc30fcad

--- ofxMSAUtils/src/ofxMSAUtils.h
+++ ofxMSAUtils/src/ofxMSAUtils.h
@@ -1,5 +1,5 @@
 /***********************************************************************
- 
+
  Copyright (c) 2008, 2009, Memo Akten, www.memo.tv
  *** The Mega Super Awesome Visuals Company ***
  * All rights reserved.
@@ -12,22 +12,22 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of MSA Visuals nor the names of its contributors 
+ *     * Neither the name of MSA Visuals nor the names of its contributors
  *       may be used to endorse or promote products derived from this software
  *       without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
  * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * ***********************************************************************/ 
+ * ***********************************************************************/
 
 #pragma once
 
@@ -39,8 +39,8 @@ void msaConstrain(float &pos, float &vel, float min, float max, float bounce = 1
 void msaConstrain(ofPoint &pos, ofPoint &vel, ofPoint &min, ofPoint &max, float bounce = 1);
 
 void msaDrawFPS();
-void msaDrawFPS(int x, int y);	
-void msaDrawFPS(int color);	
+void msaDrawFPS(int x, int y);
+void msaDrawFPS(int color);
 
 void msaSetCursor(bool forceOn = false);
 
@@ -50,42 +50,50 @@ void msaDrawQuadAtCenter();
 void msaDrawTexture(GLuint texId, GLenum textureTarget = GL_TEXTURE_2D);
 
 
+class msaImage : public ofImage {
+public:
+    ofPixels &getPixelsData() {
+        return myPixels;
+    }
+};
+
+
 class msaColor : public ofColor{
 public:
-	
+
 	void set(float r, float g, float b, float a = 1) {
 		this->r = r;
 		this->g = g;
 		this->b = b;
 		this->a = a;
 	}
-	
+
 	void setGL() {
 		glColor4f(r, g, b, a);
 	}
-	
+
 	void setClamp(float r, float g, float b, float a = 1) {
 		set(r, g, b, a);
 		clamp();
 	}
-	
+
 	void clamp() {
 		if(r > 1) r = 1;
 		if(g > 1) g = 1;
 		if(b > 1) b = 1;
 		if(a > 1) a = 1;
 	}
-	
+
 	msaColor lerpTo(const msaColor& target, float t ) {
 		return (*this * t) + (target * (1-t));
 	}
-	
-	
+
+
 	float getLuminance() {
 		return (0.2126*r) + (0.7152*g) + (0.0722*b);
 	}
-	
-	
+
+
 	// H [0..360], S and V [0..1]
 	void setHSV(float h, float s, float v, float a = 1) {
 		h = int(h) % 360;
@@ -94,8 +102,8 @@ public:
 		float p = v * (1 - s);
 		float q = v * (1 - s * f);
 		float t = v * (1 - (1 - f) * s);
-		
-		switch (i) {   
+
+		switch (i) {
 			case 0: set(v, t, p, a); break;
 			case 1: set(q, v, p, a); break;
 			case 2: set(p, v, t, a); break;
@@ -103,14 +111,14 @@ public:
 			case 4: set(t, p, v, a); break;
 			case 5: set(v, p, q, a); break;
 		}
-	} 
-	
+	}
+
 	// assumes RGB is normalized [0..1]
 	// returns H [0..360], S and V [0..1]
 	void getHSV(ofPoint& outHSV) {
 		float h, s, v;
 		float y, r1,g1,b1;
-		
+
 		v = r;
 		if (v<g) v=g;
 		if (v<b) v=b;
@@ -144,40 +152,40 @@ public:
 		if (h >= 360.) h = h-360.;
 		outHSV.set(h, s, v);
 	}
-	
-	
+
+
 	msaColor( float r=1.0f, float g=1.0f, float b=1.0f, float a=1.0f ) {
 		set(r, g, b, a);
     }
-	
+
     msaColor( const msaColor & col){
 		set(col.r, col.g, col.b, col.a);
     }
-	
-	
-	
+
+
+
     //equalitg
     bool operator==( const msaColor& col ) {
         return (r == col.r) && (g == col.g) && (b == col.b) && (a == col.a);
     }
-	
+
 	//inequalitg
     bool operator!=( const msaColor& col ) {
         return (r != col.r) || (g != col.g) || (b != col.b) || (a != col.a);
     }
-	
+
 	//Set
 	msaColor & operator=( const msaColor& col ){
 		set(col.r, col.g, col.b, col.a);
 		return *this;
 	}
-	
+
 	// Add
     msaColor operator+( const msaColor& col ) const {
         return msaColor( r+col.r, g+col.g, b+col.b, a+col.a );
     }
-	
-	
+
+
 	msaColor & operator+=( const msaColor& col ) {
         r += col.r;
         g += col.g;
@@ -185,7 +193,7 @@ public:
 		a += col.a;
         return *this;
     }
-	
+
 	msaColor & operator+=( const float & val ) {
         r += val;
         g += val;
@@ -193,12 +201,12 @@ public:
 		a += val;
         return *this;
     }
-	
+
 	// Subtract
     msaColor operator-(const msaColor& col) const {
         return msaColor( r-col.r, g-col.g, b-col.b, a-col.a);
     }
-	
+
     msaColor & operator-=( const msaColor& col ) {
         r -= col.r;
         g -= col.g;
@@ -206,7 +214,7 @@ public:
 		a -= col.a;
         return *this;
     }
-	
+
     msaColor & operator-=( const float & val ) {
         r -= val;
         g -= val;
@@ -214,12 +222,12 @@ public:
 		a -= val;
         return *this;
     }
-	
+
 	// Multiply
     msaColor operator*(const float& val) const {
         return msaColor( r*val, g*val, b*val, a*val);
     }
-	
+
     msaColor & operator*=( const msaColor& col ) {
         r*=col.r;
         g*=col.g;
@@ -227,7 +235,7 @@ public:
 		a*=col.a;
         return *this;
     }
-	
+
     msaColor & operator*=( const float & val ) {
         r*=val;
         g*=val;
@@ -235,29 +243,29 @@ public:
 		a*=val;
         return *this;
     }
-	
-	
+
+
 	// Divide
     msaColor operator/( const msaColor& col ) const {
         return msaColor( col.r!=0 ? r/col.r : r , col.g!=0 ? g/col.g : g, col.b!=0 ? b/col.b : b, col.a!=0 ? a/col.a : a);
     }
-	
+
     msaColor operator/( const float &val ) const {
 		if( val != 0){
 			return msaColor( r/val, g/val, b/val, a/val );
 		}
         return msaColor(r, g, b, a);
     }
-	
+
     msaColor& operator/=( const msaColor& col ) {
         col.r!=0 ? r/=col.r : r;
         col.g!=0 ? g/=col.g : g;
         col.b!=0 ? b/=col.b : b;
         col.a!=0 ? a/=col.a : a;
-		
+
         return *this;
     }
-	
+
     msaColor& operator/=( const float &val ) {
 		if( val != 0 ){
 			r /= val;
@@ -265,9 +273,9 @@ public:
 			b /= val;
 			a /= val;
 		}
-		
+
 		return *this;
     }
-	
-	
+
+
 };
